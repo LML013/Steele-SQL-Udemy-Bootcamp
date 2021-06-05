3788b7817fe5778c4b00abdf0786fab39070eb62
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2021-06-04 23:22:43 -0700

    Final Commit
    Put given markup and CSS into app, completed course, yay!

---
+++ triggers.sql
@@ -0,0 +1,22 @@
+/*CREATE TABLE users (
+    username VARCHAR(100),
+    age INT
+);
+
+INSERT INTO users(username,age) VALUES("Bobby", 23);
+*/
+
+DELIMITER $$
+
+CREATE TRIGGER must_be_adult
+    BEFORE INSERT ON users FOR EACH ROW
+    BEGIN
+        IF NEW.age < 18
+        THEN
+            SIGNAL SQLSTATE "45000"
+                SET MESSAGE_TEXT = "Must be an adult!";
+        END IF;
+    END;
+$$
+
+DELIMITER ;
