6f0b15993f108056bd31ce1d9e22bd69114cfe9b
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2021-06-04 20:35:16 -0700

    Add test routes
    Added the app.js file and included some test routes

---
+++ JoinUs/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+
+
+
+
+app.get("/", function(req, res){
+	res.send("WELCOME TO THE HOME PAGE");
+});
+
+app.get("/joke", function(req, res){
+	var joke = "What do you call a dog that does magic tricks? A labracadabrador.";
+	res.send(joke);
+});
+
+app.get("/random_num", function(req, res){
+	var num = Math.floor(Math.random() * 10) + 1;
+	res.send("Your lucky number is " + num + ".");
+})
+
+
+app.listen(3000, function() {
+	console.log("Server running on 3000!");
+});
+
+
+
+
+
+
+
